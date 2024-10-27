6aec05dae7e7d5ad8bcb69a21fbf6c5375025b90
Vijaya15347 <83939885+Vijaya15347@users.noreply.github.com>
2024-10-27 20:03:24 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,9 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const typed = new Typed(".text", {
+        strings: ["Full Stack Developer", "Web Developer", "JavaScript Enthusiast"],
+        typeSpeed: 100,
+        backSpeed: 100,
+        backDelay: 1000,
+        loop: true
+    });
+});
